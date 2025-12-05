4466faec760bbc60dac6a43f687248ec9f828272
sanjeevkumar-iitp <sanjeevkumar.iitp@gmail.com>
2025-12-05 21:50:46 +0530

    renamed app

--- ios/BonJoyRider/AppDelegate.swift
+++ ios/BonJoyRider/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "MyNewApp",
+      withModuleName: "BonJoy Rider",
       in: window,
       launchOptions: launchOptions
     )
