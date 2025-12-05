057d8a833738a3bedc8d906a855a372196989f81
sanjeevkumar-iitp <sanjeevkumar.iitp@gmail.com>
2025-12-05 21:50:24 +0530

    initial commit

---
+++ src/navigation/RootNavigator.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import linking from './linkingConfig';
+import { RootStackParamList } from './types';
+
+import SplashScreen from '../screens/SplashScreen';
+// import OnboardingScreen from '../screens/OnboardingScreen';
+// import LoginScreen from '../screens/LoginScreen';
+// import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const RootNavigator = () => {
+  return (
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigator;
---
+++ src/navigation/linkingConfig.ts
@@ -0,0 +1,16 @@
+import { LinkingOptions } from '@react-navigation/native';
+import { RootStackParamList } from './types';
+
+const linking: LinkingOptions<RootStackParamList> = {
+  prefixes: ['myapp://'], // your app scheme
+  config: {
+    screens: {
+      Splash: 'splash',
+      Onboarding: 'onboarding',
+      Login: 'login',
+      Home: 'home',
+    },
+  },
+};
+
+export default linking;
